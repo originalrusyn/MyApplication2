8258e9bdb10c32510ea8b1d7f27ba1e84c778cd9
m <balck.stone.666@gmail.com>
2015-04-19 15:26:54 +0300

    1

---
+++ app/src/main/java/a/A.java
@@ -0,0 +1,11 @@
+package a;
+
+import lombok.extern.slf4j.Slf4j;
+
+// @author: Mykhaylo Titov on 19.04.15 15:24.
+@Slf4j
+public class A {
+    public static void main(String[] args) {
+        log.info("ggg");
+    }
+}
