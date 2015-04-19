4e71faa849606052b09f367e0f8ec1ba6bfaad98
m <balck.stone.666@gmail.com>
2015-04-19 21:14:06 +0300

    1

--- app/src/main/java/novo/tvir/myapplication2/app/MainActivity.java
+++ app/src/main/java/novo/tvir/myapplication2/app/MainActivity.java
@@ -1,29 +1,22 @@
 package novo.tvir.myapplication2.app;
 
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import lombok.extern.slf4j.Slf4j;
-import org.androidannotations.annotations.EBean;
+import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.res.IntegerRes;
 
+@EActivity(R.layout.activity_main)
 @Slf4j
-@EBean
 public class MainActivity extends ActionBarActivity {
 
     @IntegerRes(android.R.integer.config_shortAnimTime) int shortAnimTime;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        log.info("sdfsdfsdfsdf");
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        log.info("sdfsdfsdfsdf");
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
